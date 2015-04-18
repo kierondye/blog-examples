a3ddb1f8df2d730817060f190433f53b604fa128
kierondye <kierondye@users.noreply.github.com>
2015-04-19 01:44:54 +1200

    Added custom WCF date type
    This type allows dateTime values to be serialized and deserialized
    without losing the original offset information.

---
+++ wcf-custom-type-deserialize-tsts/WcfDate.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Xml;
+using System.Xml.Schema;
+using System.Xml.Serialization;
+
+namespace DateTimeConsumingService.Contracts
+{
+    /// <summary>
+    /// Class for serving xs:date in Wcf.
+    /// http://www.codeproject.com/Articles/182960/WCF-Support-for-xs-date
+    /// </summary>
+    [XmlSchemaProvider("MySchema")]
+    public class WcfDate : IXmlSerializable
+    {
+        private DateTimeOffset theDateTimeOffset;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="WcfDate"/> class.
+        /// </summary>
+        public WcfDate()
+            : this(new DateTime(1900, 1, 1))
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="WcfDate"/> class.
+        /// </summary>
+        /// <param name="dt">The dateTime.</param>
+        public WcfDate(DateTime dt)
+        {
+            this.theDateTimeOffset = DateTime.SpecifyKind(dt, DateTimeKind.Unspecified);
+        }
+
+        /// <summary>
+        /// Detetmines the schema.
+        /// </summary>
+        /// <param name="xs">The schema set.</param>
+        /// <returns>The qualified name.</returns>
+        public static XmlQualifiedName MySchema(XmlSchemaSet xs)
+        {
+            return new XmlQualifiedName("dateTime", "http://www.w3.org/2001/XMLSchema");
+        }
+
+        /// <summary>
+        /// Gets the schema.
+        /// </summary>
+        /// <returns>Xml schema.</returns>
+        public XmlSchema GetSchema()
+        {
+            return null;
+        }
+
+        /// <summary>
+        /// Reads the XML.
+        /// </summary>
+        /// <param name="reader">The reader.</param>
+        public void ReadXml(XmlReader reader)
+        {
+            var content = reader.ReadElementContentAsString();
+            this.theDateTimeOffset = DateTimeOffset.Parse(content);
+            DateTime.SpecifyKind(this.theDateTimeOffset.DateTime, DateTimeKind.Unspecified);
+        }
+
+        /// <summary>
+        /// Writes the XML.
+        /// </summary>
+        /// <param name="writer">The writer.</param>
+        public void WriteXml(XmlWriter writer)
+        {
+            writer.WriteValue(this.theDateTimeOffset.ToString("yyyy-MM-ddTHH:mm:sszzz"));
+        }
+
+        /// <summary>
+        /// Gets the date.
+        /// </summary>
+        /// <returns>Date as DateTime.</returns>
+        public DateTime GetDate()
+        {
+            return DateTime.SpecifyKind(this.theDateTimeOffset.DateTime, DateTimeKind.Unspecified);
+        }
+
+        /// <summary>
+        /// Converts value to the string.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="System.String"/> that represents this instance.
+        /// </returns>
+        public new string ToString()
+        {
+            return this.theDateTimeOffset.ToString("yyyy-MM-ddTHH:mm:sszzz");
+        }
+    }
+}
---
+++ wcf-custom-type-deserialize-tsts/WcfDateTests.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Text;
+
+using DateTimeConsumingService.Contracts;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TimezoneTests
+{
+    [TestClass]
+    public class WcfDateTests
+    {
+        [TestMethod]
+        public void DeserializeAndSerializeDateTimeAnHourAhead()
+        {
+            // get current timezone offset + 1
+            // we want to deserialize a date from a country an hour ahead
+            // NOTE: DateTime will work out the offset of the date specified and not the current date
+            var dateTime = new DateTime(2000, 1, 1);
+            var timezoneOffsetHoursePlusOne = dateTime.Subtract(dateTime.ToUniversalTime()).Hours + 1;
+            var objectToDeserialize =
+                string.Format(
+                    "<WcfDateContract xmlns=\"http://schemas.datacontract.org/2004/07/TimezoneTests\" xmlns:i=\"http://www.w3.org/2001/XMLSchema-instance\"><Value>2000-01-01T00:00:00+{0:00}:00</Value></WcfDateContract>",
+                    timezoneOffsetHoursePlusOne);
+            // de-serialize the object
+            var serializer = new DataContractSerializer(typeof(WcfDateContract));
+            var deserializedObject =
+                serializer.ReadObject(new MemoryStream(Encoding.UTF8.GetBytes(objectToDeserialize))) as WcfDateContract;
+            // re-serialize the object
+            var memoryStream = new MemoryStream();
+            serializer.WriteObject(memoryStream, deserializedObject);
+            var serializedObject = Encoding.UTF8.GetString(memoryStream.ToArray());
+            // check that they are the same
+            Assert.AreEqual(objectToDeserialize, serializedObject);
+        }
+
+        [TestMethod]
+        public void DeserializeAndSerializeDateTimeWithCurrentOffset()
+        {
+            // get current timezone offset
+            // we want to deserialize a date from a country an hour ahead
+            // NOTE: DateTime will work out the offset of the date specified and not the current date
+            var dateTime = new DateTime(2000, 1, 1);
+            var timezoneOffsetHoursePlusOne = dateTime.Subtract(dateTime.ToUniversalTime()).Hours;
+            var objectToDeserialize =
+                string.Format(
+                    "<WcfDateContract xmlns=\"http://schemas.datacontract.org/2004/07/TimezoneTests\" xmlns:i=\"http://www.w3.org/2001/XMLSchema-instance\"><Value>2000-01-01T00:00:00{0:+00;-00;+00}:00</Value></WcfDateContract>",
+                    timezoneOffsetHoursePlusOne);
+            // de-serialize the object
+            var serializer = new DataContractSerializer(typeof(WcfDateContract));
+            var deserializedObject =
+                serializer.ReadObject(new MemoryStream(Encoding.UTF8.GetBytes(objectToDeserialize))) as WcfDateContract;
+            // re-serialize the object
+            var memoryStream = new MemoryStream();
+            serializer.WriteObject(memoryStream, deserializedObject);
+            var serializedObject = Encoding.UTF8.GetString(memoryStream.ToArray());
+            // check that they are the same
+            Assert.AreEqual(objectToDeserialize, serializedObject);
+        }
+    }
+
+    [DataContract]
+    public class WcfDateContract
+    {
+        [DataMember]
+        public WcfDate Value;
+    }
+}
